64137e4656361299f9ee3e268a0ba7394a6a13b9
kimshun0213kr <96727208+kimshun0213kr@users.noreply.github.com>
2024-07-03 16:41:15 +0900

    Update: ヘッダーの変更

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import "@/styles/globals.css";
-import { Box, Center, ChakraProvider, Heading, VStack } from "@chakra-ui/react";
+import { Box, Center, ChakraProvider, Heading, Text, VStack } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
 import Link from "next/link";
 
@@ -7,12 +7,14 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <ChakraProvider>
-        <Box w="100%" position="sticky" top="0" zIndex={"calc(infinity)"}>
+        <Box bg="#fc8819" color="white" w="100%" position="sticky" top="0" zIndex={"calc(infinity)"}>
           <Link href="/">
-            <img src="/header.png" width={"100%"} height="auto" />
+            <Center>
+              <Text fontSize={"6xl"} fontWeight={"extrabold"}>でんげき会計管理システム</Text>
+            </Center>
           </Link>
         </Box>
-        <Center>
+        <Center marginTop={"20px"}>
           <Component {...pageProps} />
         </Center>
       </ChakraProvider>
