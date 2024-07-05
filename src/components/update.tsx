65aaf8370bea0b4a4a76a31b04900a1a3f197b3e
kimshun0213kr <96727208+kimshun0213kr@users.noreply.github.com>
2024-07-06 03:34:52 +0900

    Update: 帳簿データの編集機能を作成

---
+++ src/components/update.tsx
@@ -0,0 +1,220 @@
+"useclient"
+import { Tr,Td, Select, Button, HStack, Input, Text, useToast, Tooltip } from "@chakra-ui/react";
+import { useState } from "react";
+import { EditIcon,CheckIcon, CloseIcon, DeleteIcon } from "@chakra-ui/icons"
+import { UseLoginState } from "@/hooks/UseLoginState";
+import prisma from "@/lib/prisma";
+import { useRouter } from "next/router";
+
+type updateAccount = {
+    id: number,
+    year: string,
+    date: Date,
+    type: string,
+    typeAlphabet: string,
+    subtype: string,
+    fixture: string,
+    income: number,
+    outcome: number,
+}
+
+const Update: React.FC<{ update: updateAccount }> = ({ update }) => {
+	const [isEditMode,setIsEditMode] = useState(false)
+	const [type, setType] = useState(update.type);
+	const [subType, setSubType] = useState(update.subtype);
+	const [fixture, setFixture] = useState(update.fixture);
+	const [tmpType, setTmpType] = useState(update.type);
+	const [tmpSubType, setTmpSubType] = useState(update.subtype);
+	const [tmpFixture, setTmpFixture] = useState(update.fixture);
+	const [isAdmin,isUser, Login, Logout] = UseLoginState(false);
+	const router = useRouter()
+	const toast = useToast()
+	const types: string[] = [
+		"大道具",
+		"小道具",
+		"衣装",
+		"照明",
+		"音響",
+		"庶務",
+		"その他",
+	];
+
+	async function adopt() {
+		setType(tmpType)
+		setSubType(tmpSubType)
+		setFixture(tmpFixture)
+		const getHost = await fetch("https://ipapi.co/json")
+		const res = await getHost.json()
+		const hostname = res.ip
+		const authBody = {
+			hostname
+		}
+		const token = await fetch("/api/auth/generatePass",{
+			method:"POST",
+			headers: {"Content-Type":"application/json"},
+			body: JSON.stringify(authBody)
+		})
+		const getToken = await token.json()
+		const oneTimeToken = getToken.token
+		const inputPass = localStorage.getItem("storage_token")
+		const editData = {"id":String(update.id),"year":String(update.year),"date":String(update.date),"type":String(tmpType),"typeAlphabet":alphabet[types.indexOf(tmpType)],"subtype":tmpSubType,"fixture":tmpFixture,"income":String(update.income),"outcome":String(update.outcome),"inputPass":inputPass,"oneTimeToken":oneTimeToken,"hostname":hostname}
+
+		await fetch("/api/database/updateEarning",{
+			method:"POST",
+			headers:{"Content-type":"application/json"},
+			body:JSON.stringify(editData)
+		})
+		setIsEditMode(false)
+		toast({
+			title:"編集完了",
+			status:"success",
+			duration:1500,
+			isClosable:true
+		})
+		router.push("")
+	}
+	async function aid() {
+		const id = update.id
+		const getHost = await fetch("https://ipapi.co/json")
+		const res = await getHost.json()
+		const hostname = res.ip
+		const authBody = {
+			hostname
+		}
+		const token = await fetch("/api/auth/generatePass",{
+			method:"POST",
+			headers: {"Content-Type":"application/json"},
+			body: JSON.stringify(authBody)
+		})
+		const getToken = await token.json()
+		const oneTimeToken = getToken.token
+		const inputPass = localStorage.getItem("storage_token")
+		const body = {
+			id,
+			inputPass,
+			oneTimeToken,
+			hostname
+		}
+		await fetch("/api/database/aid",{
+			method:"POST",
+			headers:{"Content-type":"application/json"},
+			body:JSON.stringify(body)
+		})
+		setIsEditMode(false)
+		toast({
+			title:"共済行き完了",
+			status:"success",
+			duration:1500,
+			isClosable:true
+		})
+		router.push("")
+	}
+	function close() {
+		setTmpType(update.type)
+		setTmpSubType(update.subtype)
+		setTmpFixture(update.fixture)
+		setIsEditMode(false)
+	}
+
+	const alphabet: string[] = ["A", "B", "C", "D", "E", "F", "X"];
+	if(isAdmin){
+		return (
+			<Tr>
+				{!isEditMode ? 
+				<>
+				<Td>{update.year}</Td>
+				<Td>{Number(new Date(update.date).getMonth())+1+"月"+String(Number(new Date(update.date).getDate()))+"日"}</Td>
+				<Td>{type}</Td>
+				<Td>{alphabet[types.indexOf(type)]}</Td>
+				<Td>{subType}</Td>
+				<Td>{fixture}</Td>
+				<Td>{update.income}</Td>
+				<Td>{update.outcome}</Td>
+				<Td><Tooltip label="編集"><EditIcon onClick={()=>setIsEditMode(true)}/></Tooltip></Td>
+				</>
+				: 
+				<>
+				<Td>{update.year}</Td>
+				<Td>{Number(new Date(update.date).getMonth())+1+"月"+String(Number(new Date(update.date).getDate()))+"日"}</Td>
+				<Td>
+					<Select
+							onChange={(e) => setTmpType(e.target.value)}
+							defaultValue={update.type}
+							>
+							<option value={"大道具"}>大道具</option>
+							<option value={"小道具"}>小道具</option>
+							<option value={"衣装"}>衣装</option>
+							<option value={"照明"}>照明</option>
+							<option value={"音響"}>音響</option>
+							<option value={"庶務"}>庶務</option>
+							<option value={"その他"}>その他</option>
+					</Select>
+				</Td>
+				<Td>{alphabet[types.indexOf(tmpType)]}</Td>
+				<Td>
+					<HStack>
+						<Select
+							onChange={(e) => setTmpSubType(e.target.value)}
+															defaultValue={tmpSubType}
+							>
+							{tmpType == "大道具" ? (
+									<>
+									<option value="1">昨年度大道具代</option>
+									<option value="2">大道具部品代</option>
+									<option value="3">大道具代</option>
+									</>
+							) : null}
+							{tmpType == "小道具" ? (
+									<>
+									<option value="1">昨年度小道具代</option>
+									<option value="2">小道具部品代</option>
+									<option value="3">小道具代</option>
+									</>
+							) : null}
+							{tmpType == "衣装" ? (
+									<>
+									<option value="1">昨年度衣装代</option>
+									<option value="2">衣装部品代</option>
+									<option value="3">衣装代</option>
+									<option value="4">化粧品代</option>
+									<option value="5">クリーニング代</option>
+									</>
+							) : null}
+							{tmpType == "照明" ? (
+									<>
+									<option value="1">昨年度照明代</option>
+									<option value="2">照明代</option>
+									</>
+							) : null}
+							{tmpType == "音響" ? (
+									<>
+									<option value="1">昨年度音響代</option>
+									<option value="2">音響代</option>
+									</>
+							) : null}
+							{tmpType == "庶務" ? (
+									<>
+									<option value="1">昨年度庶務代</option>
+									<option value="2">庶務代</option>
+									</>
+							) : null}
+							<option value="X">その他</option>
+							<option value="Z">不明</option>
+						</Select>
+						<Text>
+							{tmpSubType}
+						</Text>
+					</HStack>
+				</Td>
+				<Td><Input defaultValue={tmpFixture} onChange={(e)=>setTmpFixture(e.target.value)} /></Td>
+				<Td>{update.income}</Td>
+				<Td>{update.outcome}</Td>
+				<Td><HStack><Tooltip label="編集を保存"><CheckIcon onClick={() => adopt()} marginRight={"5px"} marginLeft={"5px"} /></Tooltip><Tooltip label="共済金からの支払いに変更"><DeleteIcon onClick={() => aid()} marginRight={"5px"} marginLeft={"5px"}/></Tooltip><Tooltip label="変更を破棄"><CloseIcon onClick={() => close()} marginRight={"5px"} marginLeft={"5px"} /></Tooltip></HStack></Td>
+				</>
+				}
+			</Tr>
+		)
+	}
+}
+
+export default Update
--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Text, VStack } from "@chakra-ui/react";
+import { Box, Button, Heading, Text, VStack } from "@chakra-ui/react";
 import Link from "next/link";
 import { UseLoginState } from "@/hooks/UseLoginState";
 import { useRouter } from "next/router";
@@ -17,17 +17,16 @@ export default function Home() {
         <Text fontSize={"2xl"}>ホーム</Text>
         {isAdmin || isUser ? (
           <>
-            <Link href={"/income"}>
-              <Box borderBottom="1px solid #fc8819">収入報告</Box>
-            </Link>
-            <Link href={"/outcome"}>
-              <Box borderBottom="1px solid #fc8819">支出報告</Box>
-            </Link>
             {isAdmin ? 
-            <Link href="/admin">
-              <Box borderBottom="1px solid #fc8819">管理者用ページ</Box>
+            <>
+            <Link href="/admin/generate">
+              <Box borderBottom="1px solid #fc8819">excel出力</Box>
             </Link>
-            : null }
+            <Link href="/admin/edit">
+              <Box borderBottom="1px solid #fc8819">帳簿データ編集</Box>
+            </Link>
+            </>
+            : <Heading>一般ユーザーの権限では使用できません。</Heading> }
             <Button onClick={Logout}>ログアウト</Button>
           </>
         ) : (
