ee359f6585e8dfbd4ceee12c90e982888f3aaa96
kimshun0213kr <96727208+kimshun0213kr@users.noreply.github.com>
2024-07-05 15:53:57 +0900

    Update: discordのhookのデータベース参照化

--- src/pages/api/discord/send.ts
+++ src/pages/api/discord/send.ts
@@ -21,13 +21,9 @@ export default async function handle(
     content: valueContent,
   };
   try {
-    const result = await prisma.threadID.findFirst({
-      where: {
-        id: 1
-      }
-    });
-    const threadID = "1257743150223462481"
-    const hookurl = "https://discord.com/api/webhooks/1206230033421836349/RolsB6o1AmgwGUTe_ns89kIQCMkAvbdjpaY1OJ0Lxk4HrOg9TRw5Xjp-uBqUmzzMh9LC?thread_id="+threadID;
+    const result = await prisma.threadID.findFirst({});
+    const hookurl = result?.hookurl+"?thread_id="+result?.threadID
+    console.log(hookurl)
     const formData = new FormData();
     formData.append("payload_json", JSON.stringify(sendData));
     const response = await fetch(hookurl, {
