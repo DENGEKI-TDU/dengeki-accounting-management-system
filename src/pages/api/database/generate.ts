c431d9986169a93d286e32e1ada125629f3f144a
kimshun0213kr <96727208+kimshun0213kr@users.noreply.github.com>
2024-08-02 17:24:20 +0900

    Update: fetchから一部axiosに変更

--- src/pages/api/database/generate.ts
+++ src/pages/api/database/generate.ts
@@ -1,12 +1,6 @@
 import prisma from "@/lib/prisma";
-import { createHash } from "crypto";
 import { NextApiResponse } from "next";
-
-const encryptSha256 = (str: string) => {
-	const hash = createHash("sha256");
-	hash.update(str);
-	return hash.digest().toString("base64");
-  };
+import axios from "axios";
 
 
 export default async function handle(
@@ -23,99 +17,92 @@ export default async function handle(
 ) {
   const { year,inputPass, oneTimeToken, hostname, from } = req.body;
   const allowHOST = process.env.NEXT_PUBLIC_ALLOW_HOSTNAME!
-  let isAdmin = false;
   const sessionToken = inputPass
-  const body = {
-    oneTimeToken
+  let http = "http"
+  if(process.env.NODE_ENV == "production"){
+	http = "https"
   }
-  const pr = await fetch("https://"+process.env.NEXT_PUBLIC_SSO_DOMAIN+"/api/auth/authToken",{
-    method:"POST",
-    headers:{"Content-Type":"application/json"},
-    body:JSON.stringify(body)
-  })
-  const passResult = await pr.json()
-  if(hostname.includes(allowHOST) && passResult){
-		const tokenLimit = passResult.limit
-		if(new Date() < new Date(tokenLimit)){
-      try {
-        const body = {
+  axios.post(http+"://"+process.env.NEXT_PUBLIC_SSO_DOMAIN+"/api/auth/authToken",{
+    oneTimeToken
+  }).then(async(pr) => {
+    const passResult = pr.data
+    if(hostname.includes(allowHOST)){
+      const tokenLimit = passResult.limit
+      if(new Date() < new Date(tokenLimit)){
+        axios.post(http+"://"+process.env.NEXT_PUBLIC_SSO_DOMAIN+"/api/auth",{
           token:sessionToken,
           mode:"get"
-        }
-        const response = await fetch("https://"+process.env.NEXT_PUBLIC_SSO_DOMAIN+"/api/auth",{
-          method: "POST",
-          headers: {"Content-Type": "application/json"},
-          body: JSON.stringify(body)
-        });
-        const data = await response.json()
-        if(data){
-          if(new Date(data.limit) > new Date()){
-            isAdmin = data.isAdmin
+        }).then(async(response) => {
+          const limit = response.data.limit
+          if(new Date(limit) > new Date()){
+            response.data.isAdmin
+            if(response.data.isAdmin){
+              if(from == "main"){
+                const result = await prisma.mainAccount.findMany({
+                  where:{
+                    year:year
+                  },
+                  orderBy:{date:"asc"}
+                });
+                let response:String[] = []
+                for(let i=0;i<result.length;i++){
+                    response.push(String(JSON.stringify(result[i])))
+                }
+                res.status(200).json({"data":result});
+              }
+              if(from == "hatosai"){
+                const result = await prisma.hatosaiAccount.findMany({
+                  where:{
+                    year:year
+                  },
+                  orderBy:{date:"asc"}
+                });
+                let response:String[] = []
+                for(let i=0;i<result.length;i++){
+                    response.push(String(JSON.stringify(result[i])))
+                }
+                res.status(200).json({"data":result});
+              }
+              if(from == "clubsupport"){
+                const result = await prisma.clubsupportAccount.findMany({
+                  where:{
+                    year:year
+                  },
+                  orderBy:{date:"asc"}
+                });
+                let response:String[] = []
+                for(let i=0;i<result.length;i++){
+                    response.push(String(JSON.stringify(result[i])))
+                }
+                res.status(200).json({"data":result});
+              }
+              if(from == "alumni"){
+                const result = await prisma.alumniAccount.findMany({
+                  where:{
+                    year:year
+                  },
+                  orderBy:{date:"asc"}
+                });
+                let response:String[] = []
+                for(let i=0;i<result.length;i++){
+                    response.push(String(JSON.stringify(result[i])))
+                }
+                res.status(200).json({"data":result});
+              }
+            } else {
+              res.status(403).json({"msg":"Query error"})
+            }
           }
-        }
-      } catch(error) {
-        console.error(error)
-      }
-      if((isAdmin)){
-        if(from == "main"){
-          const result = await prisma.mainAccount.findMany({
-            where:{
-              year:year
-            },
-            orderBy:[{income:"desc"},{date:"asc"}]
-          });
-          let response:String[] = []
-          for(let i=0;i<result.length;i++){
-              response.push(String(JSON.stringify(result[i])))
-          }
-          res.json({"data":result});
-        }
-        if(from == "hatosai"){
-          const result = await prisma.hatosaiAccount.findMany({
-            where:{
-              year:year
-            },
-            orderBy:[{income:"desc"},{date:"asc"}]
-          });
-          let response:String[] = []
-          for(let i=0;i<result.length;i++){
-              response.push(String(JSON.stringify(result[i])))
-          }
-          res.json({"data":result});
-        }
-        if(from == "clubsupport"){
-          const result = await prisma.clubsupportAccount.findMany({
-            where:{
-              year:year
-            },
-            orderBy:[{income:"desc"},{date:"asc"}]
-          });
-          let response:String[] = []
-          for(let i=0;i<result.length;i++){
-              response.push(String(JSON.stringify(result[i])))
-          }
-          res.json({"data":result});
-        }
-        if(from == "alumni"){
-          const result = await prisma.alumniAccount.findMany({
-            where:{
-              year:year
-            },
-            orderBy:[{income:"desc"},{date:"asc"}]
-          });
-          let response:String[] = []
-          for(let i=0;i<result.length;i++){
-              response.push(String(JSON.stringify(result[i])))
-          }
-          res.json({"data":result});
-        }
+        }).catch((error) => {
+          res.status(403).json(error)
+        })
       } else {
-        res.status(403).end()
+        res.status(403).json({"msg":"Authentication information expired"})
       }
     } else {
-      res.status(403).json({"msg":"Authentication information expired"})
+      res.status(403).json({"msg":"Network Authentication error"})
     }
-  } else {
-    res.status(403).end()
-  }
+  }).catch((error)=> {
+    res.status(403).json(error)
+  })
 }
