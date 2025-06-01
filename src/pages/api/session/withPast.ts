83aa5d5b76f17e5a2d78107a81c6ad9e71ae47df
kimshun0213kr <96727208+kimshun0213kr@users.noreply.github.com>
2025-06-01 23:49:38 +0900

    Update: ドメインをenvから取得するように変更

--- src/pages/api/session/withPast.ts
+++ src/pages/api/session/withPast.ts
@@ -5,16 +5,16 @@ export default async function handle(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  const HomeDomain = process.env.DENGEKI_DOMAIN as string;
   const cookies = req.headers.cookie || "";
   const jwt = cookies
     .split("; ")
     .find((row) => row.startsWith("token="))
     ?.split("=")[1];
   if (jwt != "" && jwt) {
-    const response = await axios.post(
-      "http://localhost:3000/api/portal/getMembers",
-      { token: jwt }
-    );
+    const response = await axios.post(`${HomeDomain}/api/portal/getMembers`, {
+      token: jwt,
+    });
     return res.json(response.data);
   } else {
     return res.status(403);
