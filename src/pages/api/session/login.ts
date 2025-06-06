a8cd95b716154fd8e109cf450d5278aad5330565
kimshun0213kr <96727208+kimshun0213kr@users.noreply.github.com>
2025-06-07 00:41:45 +0900

    Update: SSOのドメインを更新

--- src/pages/api/session/login.ts
+++ src/pages/api/session/login.ts
@@ -11,7 +11,7 @@ export default async function handler(
   res: NextApiResponse
 ) {
   const { name, password } = req.body;
-  const fetchResponse = await fetch("https://sso-express.vercel.app/login/", {
+  const fetchResponse = await fetch("https://sso.dengeki-fox.net/login/", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ name: name, password: password }),
